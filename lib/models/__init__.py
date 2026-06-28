1a21c638e8574f418d110ea49a54f15d32da8cca
highphysicist <kolte@usc.edu>
2026-06-28 13:08:30 -0700

    Update __init__.py, a little unclean code

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -13,7 +13,14 @@ def load_model(opt, dataloader):
         [model] -- Returned model
     """
     model_name = opt.model
-    model_path = f"lib.models.{model_name}"
-    model_lib  = importlib.import_module(model_path)
-    model = getattr(model_lib, model_name.title())
+    if model_name == 'skipganomaly':
++        from lib.models.skipganomaly import Skipganomaly
++        return Skipganomaly(opt, data)
++    if model_name == 'skipganomaly_pp':
++        from lib.models.skipganomaly_pp import SkipganomalyPP
++        return SkipganomalyPP(opt, data)
++    if model_name == 'ensemble':
++        from lib.models.ensemble import SkipGanomalyEnsemble
++        return SkipGanomalyEnsemble(opt, data)
++    raise ValueError(f'Unknown model {model_name}')
     return model(opt, dataloader)
